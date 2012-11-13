c4bd4f0e7dcca1007fd422f63f4079f62bc6f787
siyeon <lllunelll@gmail.com>
2012-11-13 20:03:06 +0900

    ajax autocomplete예제 file name 변경

---
+++ AjaxAutocomplete/src/kr/ac/mju/dislab/autocomplete/ContrySuggestServlet.java
@@ -0,0 +1,259 @@
+package kr.ac.mju.dislab.autocomplete;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ContrySuggest
+ */
+@WebServlet("/ContrySuggestServlet")
+public class ContrySuggestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	// 국가 목록 배열(아래 URI에서 LIST를 받을수 있음.)
+	// http://www.ustrem.org/en/articles/array-list-countries-en/
+	private String country_list[] = {
+			"Afghanistan",
+			"Albania",
+			"Algeria",
+			"Andorra",
+			"Angola",
+			"Antigua and Barbuda",
+			"Argentina",
+			"Armenia",
+			"Australia",
+			"Austria",
+			"Azerbaijan",
+			"Bahamas",
+			"Bahrain",
+			"Bangladesh",
+			"Barbados",
+			"Belarus",
+			"Belgium",
+			"Belize",
+			"Benin",
+			"Bhutan",
+			"Bolivia",
+			"Bosnia and Herzegovina",
+			"Botswana",
+			"Brazil",
+			"Brunei",
+			"Bulgaria",
+			"Burkina Faso",
+			"Burundi",
+			"Cambodia",
+			"Cameroon",
+			"Canada",
+			"Cape Verde",
+			"Central African Republic",
+			"Chad",
+			"Chile",
+			"China",
+			"Colombi",
+			"Comoros",
+			"Congo (Brazzaville)",
+			"Congo",
+			"Costa Rica",
+			"Cote d'Ivoire",
+			"Croatia",
+			"Cuba",
+			"Cyprus",
+			"Czech Republic",
+			"Denmark",
+			"Djibouti",
+			"Dominica",
+			"Dominican Republic",
+			"East Timor (Timor Timur)",
+			"Ecuador",
+			"Egypt",
+			"El Salvador",
+			"Equatorial Guinea",
+			"Eritrea",
+			"Estonia",
+			"Ethiopia",
+			"Fiji",
+			"Finland",
+			"France",
+			"Gabon",
+			"Gambia, The",
+			"Georgia",
+			"Germany",
+			"Ghana",
+			"Greece",
+			"Grenada",
+			"Guatemala",
+			"Guinea",
+			"Guinea-Bissau",
+			"Guyana",
+			"Haiti",
+			"Honduras",
+			"Hungary",
+			"Iceland",
+			"India",
+			"Indonesia",
+			"Iran",
+			"Iraq",
+			"Ireland",
+			"Israel",
+			"Italy",
+			"Jamaica",
+			"Japan",
+			"Jordan",
+			"Kazakhstan",
+			"Kenya",
+			"Kiribati",
+			"Korea, North",
+			"Korea, South",
+			"Kuwait",
+			"Kyrgyzstan",
+			"Laos",
+			"Latvia",
+			"Lebanon",
+			"Lesotho",
+			"Liberia",
+			"Libya",
+			"Liechtenstein",
+			"Lithuania",
+			"Luxembourg",
+			"Macedonia",
+			"Madagascar",
+			"Malawi",
+			"Malaysia",
+			"Maldives",
+			"Mali",
+			"Malta",
+			"Marshall Islands",
+			"Mauritania",
+			"Mauritius",
+			"Mexico",
+			"Micronesia",
+			"Moldova",
+			"Monaco",
+			"Mongolia",
+			"Morocco",
+			"Mozambique",
+			"Myanmar",
+			"Namibia",
+			"Nauru",
+			"Nepa",
+			"Netherlands",
+			"New Zealand",
+			"Nicaragua",
+			"Niger",
+			"Nigeria",
+			"Norway",
+			"Oman",
+			"Pakistan",
+			"Palau",
+			"Panama",
+			"Papua New Guinea",
+			"Paraguay",
+			"Peru",
+			"Philippines",
+			"Poland",
+			"Portugal",
+			"Qatar",
+			"Romania",
+			"Russia",
+			"Rwanda",
+			"Saint Kitts and Nevis",
+			"Saint Lucia",
+			"Saint Vincent",
+			"Samoa",
+			"San Marino",
+			"Sao Tome and Principe",
+			"Saudi Arabia",
+			"Senegal",
+			"Serbia and Montenegro",
+			"Seychelles",
+			"Sierra Leone",
+			"Singapore",
+			"Slovakia",
+			"Slovenia",
+			"Solomon Islands",
+			"Somalia",
+			"South Africa",
+			"Spain",
+			"Sri Lanka",
+			"Sudan",
+			"Suriname",
+			"Swaziland",
+			"Sweden",
+			"Switzerland",
+			"Syria",
+			"Taiwan",
+			"Tajikistan",
+			"Tanzania",
+			"Thailand",
+			"Togo",
+			"Tonga",
+			"Trinidad and Tobago",
+			"Tunisia",
+			"Turkey",
+			"Turkmenistan",
+			"Tuvalu",
+			"Uganda",
+			"Ukraine",
+			"United Arab Emirates",
+			"United Kingdom",
+			"United States",
+			"Uruguay",
+			"Uzbekistan",
+			"Vanuatu",
+			"Vatican City",
+			"Venezuela",
+			"Vietnam",
+			"Yemen",
+			"Zambia",
+			"Zimbabwe"
+	};
+
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ContrySuggestServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html");
+
+		String query=request.getParameter("query");
+		PrintWriter out=response.getWriter();
+
+		if (query == null){
+			out.println("<div>Cannot access</div>");
+
+		}
+		else{
+
+			if(query.length() > 0)
+			{
+				for( String country : country_list )
+				{
+					if( country.contains(query))
+					{	
+						out.println("<li onclick=\"fill('"+country+"');\">"+country+"</li>"); 
+					}
+				}
+			}
+		}
+		out.close();
+	}
+
+}
