9a96e7cd02b44c29c63843ac010e098d954cbbc4
hanbyul <hbgod92@nate.com>
2014-10-20 16:51:02 +0900

    data.js imageUrl modify and daumAPIKey modify

--- Mashup_Map/data.js
+++ Mashup_Map/data.js
@@ -1,35 +1,37 @@
-var data = [
+﻿var data = [
 		{name:"박영배", addr:"서울 서초구 반포본동 반포아파트 103-201호", position: [37.504337403, 126.9906757412]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/parkyb.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19811018.gif"},
 		{name:"윤병주", addr:"경기 용인시 처인구 양지면 송문리 291-11",  position:[37.2175583, 127.2543126]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/yoonbj.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19821010.gif"},
 		{name:"김상운", addr:"경기 용인시 기흥구 신갈동 697 녹원마을 새천년그린빌 502-1204호",  position:[37.2879984, 127.1189434]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/kimsw.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19891005.gif"},
 		{name:"주우석", addr:"경기 용인시 기흥구 영덕동 흥덕마을13단지 경남아너스빌 1307-1003호",  position:[37.27489673, 127.091916986]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/joows.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19921307.gif"},
 		{name:"장혁수", addr:"경기 용인시 처인구 해곡동 49-1 라호",  position: [37.1798837, 127.2737156]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/janghs.gif"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19921310.gif"},
 		{name:"최성운", addr:"서울 강남구 대치2동 미도아파트 211-204호",  position:[37.493981, 127.069161424]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/choisw.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19931007.gif"},,
 		{name:"홍석원", addr:"경기 수원시 영통구 망포동 LG동수원자이아파트 303-1305호",  position:[37.2366876, 127.0566934]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/hongsw.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19951352.gif"},
 		{name:"전종훈", addr:"경기 성남시 분당구 정자동 아이파크분당아파트 102-1501호",  position:[37.3711555, 127.1051706]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/chunjh.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19951915.gif"},
 		{name:"이충기", addr:"서울 강남구 역삼2동 777-35",  position: [37.495055, 127.041421]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/leeck.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19961313.gif"},
 		{name:"박현민", addr:"경기 성남시 분당구 서현동 87 시범단지삼성아파트 108-2702호",  position: [37.3833774, 127.1283307]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/parkhm.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/19961318.gif"},
 		{name:"조세형", addr:"경기 성남시 분당구 구미동 까치마을 신원아파트 301-1501호",  position:[37.349286, 127.1139902]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/chosh.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/20001302.gif"},
 		{name:"이강선", addr:"서울 광진구 화양동 현대아파트 102-1801호",  position: [37.5444835, 127.0656838]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/leeks.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/20001305.gif"},
 		{name:"류연승", addr:"경기 용인시 수지구 풍덕천2동 신정마을 현대프라임아파트 201-1201호",  position: [37.3235109, 127.0902154]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/ryuys.png"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/20030048.gif"},
 		{name:"이명호", addr:"서울 서초구 반포4동 101-8 신동광빌라 201호",  position:[37.4955083, 126.9972618]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/leemh.gif"},
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/20040032.gif"},
 		{name:"권동섭", addr:"경기 용인시 기흥구 중동 호수마을 상록롯데캐슬아파트 1003-1804호",  position:[37.2791564, 127.1549882]
-, photo:"http://cs.mju.ac.kr/external/introduction/images/picture/kyunds.gif"},
-		{name:"김상균", addr:"경기 용인시 수지구 죽전동 한라신영프로방스1차아파트 603-1204호",  position:[37.3217894, 127.114352], photo:"http://cs.mju.ac.kr/external/introduction/images/picture/kimsk.gif"},
-		{name:"한승철", addr:"경기 과천시 과천동 140-40", position:[37.4475582, 127.0092987], photo:"http://cs.mju.ac.kr/external/introduction/images/picture/hansc.gif"}
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/20060650.gif"},
+		{name:"김상균", addr:"경기 용인시 수지구 죽전동 한라신영프로방스1차아파트 603-1204호",  position:[37.3217894, 127.114352]
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/20070075.gif"},
+		{name:"한승철", addr:"경기 과천시 과천동 140-40", position:[37.4475582, 127.0092987]
+, photo:"https://myiweb.mju.ac.kr/upload/adm/aps/20080029.gif"}
 		
 	];
